2da8fc09b307e6ebaf688b6faaf208e644db26c9
elaskowski <evalaskows@gmail.com>
2018-07-05 15:15:04 -0700

    Node solution v3

--- server/helpers.js
+++ server/helpers.js
@@ -1,10 +1,3 @@
-//Let's first add in our headers. These will allow Cross-Origin resource
-//sharing (CORS)
-//It allows the server to talk to websites that are on different domains
-//like our to do list file://node-review/todolist.html
-
-//TALK ABOUT WHAT THESE ARE
-
 module.exports = {
   sendResponse: (res, data, statusCode) => {
     statusCode = statusCode || 200;
